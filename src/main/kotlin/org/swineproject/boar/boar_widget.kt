e615018c0366f126b82edabf3b658dbeb3f44a67
DeflatedPickle <DeflatedPickle@gmail.com>
2018-08-24 08:12:59 +0100

    Added visual nodes

--- src/main/kotlin/org/swineproject/boar/boar_widget.kt
+++ src/main/kotlin/org/swineproject/boar/boar_widget.kt
@@ -16,9 +16,29 @@ class BoarWidget(display: Display, parent: Composite) : Canvas(parent, SWT.NONE)
                 val size = 480
                 val destX = (clientArea.width / 2) - (size / 2)
                 val destY = (clientArea.height / 2) - (size / 2)
-                
+
                 val image = Image(display, javaClass.classLoader.getResource("sprites/pig/idle/pig_idle_0.png").path)
                 event.gc.drawImage(image, 0, 0, image.bounds.width, image.bounds.height, destX, destY, size, size)
+
+                val nodeSize = 24
+
+                event.gc.background = display.getSystemColor(SWT.COLOR_BLACK)
+                // Top Notch
+                event.gc.drawRectangle(destX, destY, size, size)
+                event.gc.fillRectangle(destX + ((size - (nodeSize / 2)) / 2), destY - (nodeSize / 2), nodeSize, nodeSize)
+
+                // Bottom Notch
+                event.gc.drawRectangle(destX, destY, size, size)
+                event.gc.fillRectangle(destX + ((size - (nodeSize / 2)) / 2), (destY + size) - (nodeSize / 2), nodeSize, nodeSize)
+
+                // Left Notch
+                event.gc.drawRectangle(destX, destY, size, size)
+                event.gc.fillRectangle(destX - (nodeSize / 2), (destY + (size / 2)) - (nodeSize / 2), nodeSize, nodeSize)
+
+                // Right Notch
+                event.gc.drawRectangle(destX, destY, size, size)
+                event.gc.fillRectangle((destX + size) - (nodeSize / 2), (destY + (size / 2)) - (nodeSize / 2), nodeSize, nodeSize)
+
                 image.dispose()
             }
         })
